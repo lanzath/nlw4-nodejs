d2fea73324b7714642d1665af6ec8f8ae700c101
Thiago Lanza <lanzathm@gmail.com>
2021-02-24 19:16:35 -0300

    Survey model, migration, controller and endpoints

---
+++ api/src/Controllers/SurveyController.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { SurveysRepository } from '../Repositories/SurveysRepository';
+
+class SurveyController
+{
+  /**
+   * Create a new record in database.
+   *
+   * @param request
+   * @param response
+   */
+  async create(request: Request, response: Response)
+  {
+    const { title, description } = request.body;
+
+    const surveysRepository = getCustomRepository(SurveysRepository);
+
+    const survey = surveysRepository.create({ title, description });
+
+    await surveysRepository.save(survey);
+
+    return response.status(201).json(survey);
+  }
+
+  /**
+   * List all records in database.
+   *
+   * @param request
+   * @param response
+   */
+  async index(request: Request, response: Response)
+  {
+    const surveysRepository = getCustomRepository(SurveysRepository);
+
+    const surveys = await surveysRepository.find();
+
+    return response.status(200).json(surveys);
+  }
+}
+
+export { SurveyController }
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import { SurveyController } from './Controllers/SurveyController';
 import { UserController } from './Controllers/UserController';
 
 const router = Router();
-const userController = new UserController();
 
+const userController = new UserController();
+const surveyController = new SurveyController();
+
+// Users endpoints
 router.post('/users', userController.create);
+
+// Surveys endpoints
+router.post('/surveys', surveyController.create);
+router.get('/surveys', surveyController.index);
 
 export { router }

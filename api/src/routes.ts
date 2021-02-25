a96bf76937a233d4922a57983f70439d6b6aff0f
Thiago Lanza <lanzathm@gmail.com>
2021-02-25 18:29:32 -0300

    Endpoint to mail send

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { SurveyController } from './Controllers/SurveyController';
 import { UserController } from './Controllers/UserController';
+import { SendMailController } from './Controllers/SendMailController';
 
 const router = Router();
 
 const userController = new UserController();
 const surveyController = new SurveyController();
+const sendMailController = new SendMailController();
 
 // Users endpoints
 router.post('/users', userController.create);
@@ -13,5 +15,8 @@ router.post('/users', userController.create);
 // Surveys endpoints
 router.post('/surveys', surveyController.create);
 router.get('/surveys', surveyController.index);
+
+// Send mail endpoints
+router.post('/send-email', sendMailController.execute);
 
 export { router }

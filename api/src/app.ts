c3f67759c7fb811feb0194517f7e3168e3edc4ae
Thiago Lanza <lanzathm@gmail.com>
2021-02-26 18:13:33 -0300

    App error treatment implemented

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import createConnection from './database';
 import { router } from './routes';
+import { AppError } from './errors/AppError';
 
 createConnection();
 const app = express();
@@ -9,5 +11,16 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(router);
+
+app.use((error: Error, request: Request, response: Response, _next: NextFunction) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({ message: error.message });
+  }
+
+  return response.status(500).json({
+    status: 'Error',
+    message: `Internal server error ${error.message}`
+  });
+});
 
 export { app }

8521ad78f2a7982cd06b173cc60a3be9d3b29da5
Thiago Lanza <lanzathm@gmail.com>
2021-02-26 18:14:31 -0300

    Validation added

--- api/src/Controllers/UserController.ts
+++ api/src/Controllers/UserController.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
+import * as yup from 'yup';
+
+import { AppError } from './../errors/AppError';
 import { UsersRepository } from '../Repositories/UsersRepository';
 
 class UserController {
@@ -12,11 +15,23 @@ class UserController {
   async create(request: Request, response: Response) {
     const { name, email } = request.body;
 
+    // request validation
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required()
+    });
+
+    try {
+      await schema.validate(request.body);
+    } catch (error) {
+      throw new AppError(error);
+    }
+
     const usersRepository = getCustomRepository(UsersRepository);
 
     const userAlreadyExists = await usersRepository.findOne({email});
     if (userAlreadyExists) {
-      return response.status(409).json({error: 'user already exists.'})
+      throw new AppError('User already exists', 409);
     }
 
     const user = usersRepository.create({ name, email });

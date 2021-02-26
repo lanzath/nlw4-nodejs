5a298e0c7b6610894a0a00f3629bdb6ec0fbd941
Thiago Lanza <lanzathm@gmail.com>
2021-02-26 18:13:48 -0300

    New endpoints

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,22 +1,33 @@
 import { Router } from 'express';
+
 import { SurveyController } from './Controllers/SurveyController';
 import { UserController } from './Controllers/UserController';
 import { SendMailController } from './Controllers/SendMailController';
+import { AnswerController } from './Controllers/AnswerController';
+import { NpsController } from './Controllers/NpsController';
 
 const router = Router();
 
 const userController = new UserController();
 const surveyController = new SurveyController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
+const npsController = new NpsController();
 
-// Users endpoints
+// Users endpoint
 router.post('/users', userController.create);
 
 // Surveys endpoints
 router.post('/surveys', surveyController.create);
 router.get('/surveys', surveyController.index);
 
-// Send mail endpoints
+// Send mail endpoint
 router.post('/send-email', sendMailController.execute);
+
+// Survey user endpoint
+router.get('/answers/:value', answerController.execute);
+
+// NPS info endpoint
+router.get('/nps/:survey_id', npsController.execute);
 
 export { router }
